2f4a5cc5d01961eb20cf1ffcc6b3453fc0909c8f
cga00037 <99319104+cga00037@users.noreply.github.com>
2022-04-16 12:17:56 +0200

    llegadaAsentamiento

--- games/media/games/tutorial/nuclearwintertale.game.en.js
+++ games/media/games/tutorial/nuclearwintertale.game.en.js
@@ -45,7 +45,7 @@ undum.game.situations = {
 		<p>Con todo esto hace ya varios años que sabes por rumores de los guardias del refugio que el gobernador del refugio periódicamente comercia con caravanas del exterior y que este es relativamente habitable, pues si no, nadie quedaría fuera para comerciar.\
 		Desde hace semanas llevas preparándote para escapar de aquí con tu <a href='./mejora-fuerza'>Fuerza</a> o <a href='./mejora-destreza'>Destreza </a>\
 		y tu <a href='./mejora-inteligencia'>Inteligencia</a> o <a href='./mejora-suerte'>Suerte</a> además de varias herramientas preparas un <a href='hub'>plan de huída.</a></p>\
-		\
+		<p><a href='caminoasentamiento1'>Camino al asentamiento</a></p>\
 		",
 		{
 			actions: {
@@ -129,6 +129,159 @@ undum.game.situations = {
             },
 	}
 	),
+	
+	caminoasentamiento1: new undum.SimpleSituation(
+        "<h1>Camino al asentamiento</h1>\
+		<p>Intrigado por ver qué clase de cosas hay en ese asentamiento, decides poner rumbo hacía\
+		allí. A lo lejos ves como una caravana de personas se acerca al asentamiento con\
+		intención de entrar. Distraído por aquella visión te tropiezas con una raíz mutante que\
+		nace del suelo con la mala suerte de que caes rodando por una cuesta hacia abajo. Te\
+		levantas del suelo flipando de lo tonto que eres, y te percatas que al fondo del camino\
+		que conecta con la cuesta hay una especie de agujero en la pared. Tienes curiosidad por\
+		saber qué puede haber en el agujero, por lo que vas a ver que hay dentro. Está demasiado\
+		oscuro pero puedes intuir una especie de esqueleto de humanoide con 4 piernas, el cual\
+		contiene en su mano algo que brilla gracias al reflejo de la poca luz que entra en el agujero.\
+		Te acercas, le abres la mano al esqueleto y <a class='once' href='./chapa'>coges lo que tiene en su interior</a>,\
+		o haces caso a tu querida tía con los peligros del yermo y <a class ='once' href='./nada'>no tocas nada</a>.</p>\
+		",
+		{
+			actions: {
+                "chapa": function (character, system, action) {
+					system.setQuality("chapaDorada", true);
+                    system.write("<p>Coges lo que tiene en su interior, es una chapa dorada que no\
+					sabes para qué sirve, así que optas por guardarla en tu bolsillo. Al sacar tu\
+					mano del bolsillo notas un escozor que te quema la mano. Te fijas más detenidamente\
+					y te está saliendo un sarpullido con no muy buena pinta</p>"); //FIXME: cambiar el css para que p no salga con un hueco al principio
+					system.setQuality("destreza", character.qualities.skill-1);  //FIXME: cambiar la iluminacion a rojo 
+					system.setQuality("sarpullido", true);
+					system.setCharacterText("<p>Añades chapa dorada a tu inventario</p>\
+					<p>Conseguiste un sarpullido radiactivo, enhorabuena</p>\
+					<p>Has perdido destreza por el sarpullido</p>");					
+					system.doLink("caminoasentamiento2");
+                },
+				"nada": function (character, system, action) {
+                    system.write("<p>No tocas nada.</p>");
+					system.doLink("caminoasentamiento2");
+                }
+            }
+		}
+    ),
+	
+	caminoasentamiento2: new undum.SimpleSituation(
+        "<p>Justo antes de marcharte te fijas que en el suelo entre las piernas del esqueleto\
+		hay una especie de cilindro gris. Decides <a class='once' href='./linterna'>cogerlo</a> sin tocar nada más\
+		o <a class='once' href='./nada'>no lo coges</a> puesto que parece no tener ninguna importancia.</p>\
+		",
+		{
+			actions: {
+                "linterna": function (character, system, action) {
+					system.setQuality("linterna", true);
+                    system.setCharacterText("<p>Añades linterna a tu inventario</p>");
+                    system.write("<p>Decides cogerlo sin tocar nada más. Lo miras bien, observas que\
+					el cilindro tiene un botón que no puedes evitar pulsar y ves como sale del aparato un\
+					destello que dura un segundo, parece ser una especie de linterna. Sigues pulsando el\
+					botón pero no notas ningún cambio, así que te lo guardas por si acaso.</p>");
+					system.doLink("entradaasentamiento");
+                },
+				"nada": function (character, system, action) {
+                    system.write("<p>No lo coges.</p>");
+					system.doLink("entradaasentamiento");
+                }
+            }
+		}
+    ),
+	
+	entradaasentamiento: new undum.SimpleSituation(
+        "<h1>Entrada al asentamiento</h1>\
+		<p>Te pones de nuevo en marcha hasta llegar al asentamiento, y una vez allí, te encuentras\
+		con los guardianes de la puerta que te miran de arriba a abajo. Al verte con esa ropa\
+		característica del refugio donde has vivido toda tu vida, se miran con una sonrisa malévola y\
+		te dicen que para pasar necesitas pagar la entrada. No esperabas que hubiera que pagar\
+		para entrar, por lo que echas mano a <a class='once' href='./bolsillo'>tus bolsillos</a>.\
+		</p>\
+		",
+		{
+			actions: {
+                "bolsillo": function (character, system, action) {
+					if (character.qualities.chapaDorada == true) {
+						system.write("<p><a href='./chapa'>Les puedes dar la chapa dorada</a> que encontraste anteriormente,\
+						también <a href='./suerte'>te puedes inventar una historia trágica</a> de algo que nunca te sucedió,\
+						para dar pena y poder así entrar, o <a href='./entrar'>te vas de allí</a> blasfemando en voz baja.\
+						</p>");
+					}else{
+						system.write("<p>Puesto que no tienes nada, <a href='./suerte'>te inventas una historia trágica</a> de algo\
+						que nunca te sucedió, para dar pena y poder así entrar, o <a href='./entrar'>te vas\
+						de allí</a> blasfemando en voz baja.</p>");	
+					}
+                },
+				"chapa": function (character, system, action) {
+					system.setQuality("chapaDorada", false);
+					system.setCharacterText("<p>Has perdido la chapa dorada</p>");
+					system.write("<p>Les ofreces la chapa dorada y te permiten el paso sin reprochar.</p>");
+					system.doLink("asentamiento");
+                },
+				"suerte": function (character, system, action) {
+					if(character.qualities.suerte >= 3){
+						system.write("<p>Es tu día e suerte, tu historia hace llorar a los guardias, y te\
+						permiten el paso al asentamiento.</p>");
+						system.doLink("asentamiento");
+					}else{
+						system.write("<p>Tu historia no ha colado, has quedado como un pendejo mentiroso, por\
+						lo que te dan una patada y te echan de allí como si fueras un perro pulgoso</p>");
+						system.doLink("./entrar");
+					}
+                },
+				"entrar": function (character, system, action) {
+					system.write("<p> Después de tu primer intento fallido de entrar en el asentamiento, buscas otra manera\
+						de colarte. Las murallas del asentamiento están construidas con chatarra y madera\
+						en mal estado como si de una barricada mal hecha se tratara, por lo que decides rodear\
+						la muralla en busca de alguna apertura lo suficientemente grande para colarte.\
+						Rodeas las murallas hasta que ves un agujero que es bastante pequeño pero que con un\
+						poquito de esfuerzo crees que puedes entrar. Así pues, miras a tu alrededor antes de\
+						entrar por si alguien te puede estar viendo. Al no haber moros en la costa procedes a\
+						colarte por el agujero con precaución de no pincharte con ningún hierro o madera\
+						putrefacta. Lo estás haciendo muy bien hasta que escuchas un ruido afuera por donde\
+						rodeaste la muralla, por lo que te das prisa en atravesar el dichoso agujero.</p>");
+					system.doLink("entrada2/entrar");
+                }
+            }
+		}
+    ),
+	entrada2: new undum.SimpleSituation(
+        "",
+		{
+			actions: {
+                "entrar": function (character, system, action) {
+					if (character.qualities.destreza > 2) {
+						system.write("<p> Lo atraviesas a toda velocidad con la mala suerte de que rajas\
+						tu traje del refugio.</p>");
+						system.write("<p><a href='asentamiento'>Sigamos</a></p>");
+					}else{
+						//system.delay(0.1);
+						system.write("<p>Lo atraviesas a toda velocidad con la mala suerte de que te rajas\
+						el hombro y tu traje del refugio con un metal afilado que sobresalía de la muralla. Te duele\
+						y notas como tu brazo ha perdido fuerza\
+						pero no le das más importancia, lo importante es que has conseguido entrar</p>");
+						system.setQuality("fuerza", character.qualities.skill-1);
+						system.setCharacterText("<p>Has perdido fuerza por la herida</p>");
+						system.write("<p><a href='asentamiento'>Sigamos</a></p>");
+					}
+                }
+            }
+			//optionText: "",
+			//canView: function(character, system, host) {
+            //    return character.qualities.destreza > 2;
+            //}
+		}
+    ),
+	asentamiento: new undum.SimpleSituation(
+        "<h1>Asentamiento</h1>\
+		<p>Asentamiento</p>\
+		",
+		{
+			
+		}
+    ),
 };
 
 // ---------------------------------------------------------------------------
@@ -151,7 +304,16 @@ undum.game.qualities = {
     ),
     suerte: new undum.NumericQuality(
         "Suerte", {priority:"0004", group:'stats'}
-    )
+    ),
+	sarpullido: new undum.OnOffQuality(
+        "Sarpullido", { priority: "0005", group: 'stats', onDisplay: "&#9762;" }
+    ),
+	chapaDorada: new undum.OnOffQuality(
+        "Chapa Dorada", { priority: "0001", group: 'inventario', onDisplay: "&#10003;" }
+    ),
+	linterna: new undum.OnOffQuality(
+        "Linterna", { priority: "0002", group: 'inventario', onDisplay: "&#10050;" }
+    ),
 };
 
 // ---------------------------------------------------------------------------
@@ -161,7 +323,8 @@ undum.game.qualities = {
  * the end. It is an error to have a quality definition belong to a
  * non-existent group. */
 undum.game.qualityGroups = {
-    stats: new undum.QualityGroup(null, {priority:"0001"})
+    stats: new undum.QualityGroup(null, {priority:"0001"}),
+	inventario: new undum.QualityGroup('Inventario', { priority: "0002" })
 };
 
 // ---------------------------------------------------------------------------
@@ -173,4 +336,7 @@ undum.game.init = function(character, system) {
     character.qualities.suerte = 0;
 	character.qualities.inteligencia=0;
     system.setCharacterText("<p>Estas son tus estadísticas y objetos.</p>");
+	system.setQuality("chapaDorada", false);
+	system.setQuality("sarpullido", false);
+	system.setQuality("linterna", false);
 };
